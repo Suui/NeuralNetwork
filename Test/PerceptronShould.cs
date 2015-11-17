7f08660db3298cbdc59dffe85428cf0b6def3747
Suui <suui.gd@gmail.com>
2015-11-17 23:33:42 +0100

    Green: return_1_by_default

--- Test/PerceptronShould.cs
+++ Test/PerceptronShould.cs
@@ -1,6 +1,26 @@
-﻿
+﻿using NUnit.Framework;
+using FluentAssertions;
+
 
 namespace Test
 {
+	[TestFixture]
+	public class PerceptronShould
+	{
+		[Test]
+		public void return_1_by_default()
+		{
+			Perceptron perceptron = new Perceptron();
 
+			perceptron.ExitValue().Should().Be(1);
+		}
+	}
+
+	public class Perceptron
+	{
+		public int ExitValue()
+		{
+			return 1;
+		}
+	}
 }
