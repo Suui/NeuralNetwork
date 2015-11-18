4515639fbb4941589054a3e3b4f9b3d858a62447
Suui <suui.gd@gmail.com>
2015-11-18 02:59:49 +0100

    Refactor parallel step removed unneeded

--- Test/Connection.cs
+++ Test/Connection.cs
@@ -12,26 +12,5 @@ namespace Test
 			To = to;
 			Weight = 1.0;
 		}
-
-		protected bool Equals(Connection other)
-		{
-			return From == other.From && To == other.To;
-		}
-
-		public override bool Equals(object obj)
-		{
-			if (ReferenceEquals(null, obj)) return false;
-			if (ReferenceEquals(this, obj)) return true;
-			if (obj.GetType() != this.GetType()) return false;
-			return Equals((Connection) obj);
-		}
-
-		public override int GetHashCode()
-		{
-			unchecked
-			{
-				return (From*397) ^ To;
-			}
-		}
 	}
 }
