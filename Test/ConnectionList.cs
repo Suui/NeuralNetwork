0afc704ab3843fe072c25b856b6fad8b410659f8
Suui <suui.gd@gmail.com>
2015-11-18 03:03:01 +0100

    Refactor parallel end extracted class

---
+++ Test/ConnectionList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Test
+{
+	public class ConnectionList
+	{
+		Dictionary<Tuple<int, int>, Connection> Connections { get; } = new Dictionary<Tuple<int, int>, Connection>();
+
+		public void Add(Connection connection)
+		{
+			Connections.Add(new Tuple<int, int>(connection.From, connection.To), connection);
+		}
+
+		public Connection this[int from, int to] => Connections[new Tuple<int, int>(@from, to)];
+
+		public bool ContainsKey(int from, int to)
+		{
+			return Connections.ContainsKey(new Tuple<int, int>(from, to));
+		}
+
+		public int Count()
+		{
+			return Connections.Count;
+		}
+	}
+}
--- Test/Layer.cs
+++ Test/Layer.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-
-namespace Test
+﻿namespace Test
 {
 	public class Layer
 	{
@@ -26,27 +22,6 @@ namespace Test
 		public int Count()
 		{
 			return ConnectionList.Count();
-		}
-	}
-	public class ConnectionList
-	{
-		Dictionary<Tuple<int, int>, Connection> Connections { get; } = new Dictionary<Tuple<int, int>, Connection>();
-
-		public void Add(Connection connection)
-		{
-			Connections.Add(new Tuple<int, int>(connection.From, connection.To), connection);
-		}
-
-		public Connection this[int from, int to] => Connections[new Tuple<int, int>(@from, to)];
-
-		public bool ContainsKey(int from, int to)
-		{
-			return Connections.ContainsKey(new Tuple<int, int>(from, to));
-		}
-
-		public int Count()
-		{
-			return Connections.Count;
 		}
 	}
 }
