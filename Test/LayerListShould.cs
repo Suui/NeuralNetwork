c25857f8815ac273de5242f2057780f7ac1da272
Suui <suui.gd@gmail.com>
2015-11-18 23:25:32 +0100

    Refactor to setup

--- Test/LayerListShould.cs
+++ Test/LayerListShould.cs
@@ -10,33 +10,33 @@ namespace Test
 	[TestFixture]
 	public class LayerListShould
 	{
-		[Test]
-		public void be_built_with_the_correct_indexes()
+		public LayerList Layers { get; set; }
+
+		[SetUp]
+		public void given_a_layer_list_with_3_layers()
 		{
-			var layers = new LayerListBuilder().Build()
+			Layers = new LayerListBuilder().Build()
 											   .WithLayer(1).From(3).To(4)
 											   .WithLayer(2).From(4).To(4)
 											   .WithLayer(3).From(4).To(2)
 											   .Get();
+		}
 
-			layers.HasLayer(0).Should().BeFalse();
-			layers.HasLayer(3).Should().BeTrue();
+		[Test]
+		public void be_built_with_the_correct_indexes()
+		{
+			Layers.HasLayer(0).Should().BeFalse();
+			Layers.HasLayer(3).Should().BeTrue();
 		}
 
 		[Test]
 		public void return_layers_with_their_correct_indexes()
 		{
-			var layers = new LayerListBuilder().Build()
-											   .WithLayer(1).From(3).To(4)
-											   .WithLayer(2).From(4).To(4)
-											   .WithLayer(3).From(4).To(2)
-											   .Get();
-
-			Assert.Throws<KeyNotFoundException>(() => layers[0].Count());
-			layers[1].Count().Should().Be(12);
-			layers[2].Count().Should().Be(16);
-			layers[3].Count().Should().Be(8);
-			Assert.Throws<KeyNotFoundException>(() => layers[4].Count());
+			Assert.Throws<KeyNotFoundException>(() => Layers[0].Count());
+			Layers[1].Count().Should().Be(12);
+			Layers[2].Count().Should().Be(16);
+			Layers[3].Count().Should().Be(8);
+			Assert.Throws<KeyNotFoundException>(() => Layers[4].Count());
 		}
 	}
 }
