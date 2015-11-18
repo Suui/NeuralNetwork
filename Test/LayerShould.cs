56a0ccf562bbf271be9c4ab09d2b9c50121407f2
Suui <suui.gd@gmail.com>
2015-11-18 03:21:57 +0100

    Refactor to setup

--- Test/LayerShould.cs
+++ Test/LayerShould.cs
@@ -7,38 +7,38 @@ namespace Test
 	[TestFixture]
 	public class LayerShould
 	{
+		private static Layer Layer { get; set; }
+
+		[SetUp]
+		public void given_a_layer_from_2_to_4()
+		{
+			Layer = new Layer(2, 4);
+		}
+
 		[Test]
 		public void have_8_connections_when_built_from_2_to_4()
 		{
-			var layer = new Layer(2, 4);
-
-			layer.Count().Should().Be(8);
+			Layer.Count().Should().Be(8);
 		}
 
 		[Test]
 		public void have_the_correct_indexes()
 		{
-			var layer = new Layer(2, 4);
-
-			layer.HasConnection(2, 4).Should().BeTrue();
+			Layer.HasConnection(2, 4).Should().BeTrue();
 		}
 
 		[Test]
 		public void return_the_weight_of_a_connection()
 		{
-			var layer = new Layer(1, 1);
-
-			layer.Connection(1, 1).Weight.Should().Be(1.0);
+			Layer.Connection(1, 1).Weight.Should().Be(1.0);
 		}
 
 		[Test]
 		public void set_a_connection_for_a_weight()
 		{
-			var layer = new Layer(1, 1);
+			Layer.Connection(1, 1).Weight = 0.0;
 
-			layer.Connection(1, 1).Weight = 0.0;
-
-			layer.Connection(1, 1).Weight.Should().Be(0.0);
+			Layer.Connection(1, 1).Weight.Should().Be(0.0);
 		}
 	}
 }
