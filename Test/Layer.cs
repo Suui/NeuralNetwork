b275259b4b9d9e1e344d53c65ddf510b0a8c6f54
Suui <suui.gd@gmail.com>
2015-11-18 03:04:27 +0100

    Refactor rename

--- Test/Layer.cs
+++ Test/Layer.cs
@@ -2,26 +2,26 @@
 {
 	public class Layer
 	{
-		public ConnectionList ConnectionList { get; }
+		public ConnectionList Connections { get; }
 
 		public Layer(int leftPerceptrons, int rightPerceptrons)
 		{
-			ConnectionList = ConnectionListBuilder.Build(leftPerceptrons, rightPerceptrons);
+			Connections = ConnectionListBuilder.Build(leftPerceptrons, rightPerceptrons);
 		}
 
 		public Connection Connection(int from, int to)
 		{
-			return ConnectionList[from, to];
+			return Connections[from, to];
 		}
 
 		public bool HasConnection(int from, int to)
 		{
-			return ConnectionList.ContainsKey(from, to);
+			return Connections.ContainsKey(from, to);
 		}
 
 		public int Count()
 		{
-			return ConnectionList.Count();
+			return Connections.Count();
 		}
 	}
 }
