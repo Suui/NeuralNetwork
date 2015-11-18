71fe498f8ef950a6c13b103b725f07b413376262
Suui <suui.gd@gmail.com>
2015-11-18 21:45:42 +0100

    Refactor parallel step

--- Test/LayerListShould.cs
+++ Test/LayerListShould.cs
@@ -12,12 +12,7 @@ namespace Test
 		[Test]
 		public void be_built_with_the_correct_indexes()
 		{
-			LayerList layers = new LayerList()
-									.With(new Layer(3, 4))
-									.With(new Layer(4, 4))
-									.With(new Layer(4, 2));
-
-			layers = new LayerList()
+			var layers = new LayerList()
 							.WithLayer(1).From(3).To(4)
 							.WithLayer(2).From(4).To(4)
 							.WithLayer(3).From(4).To(2);
@@ -29,10 +24,10 @@ namespace Test
 		[Test]
 		public void return_layers_with_their_correct_indexes()
 		{
-			LayerList layers = new LayerList()
-									.With(new Layer(3, 4))
-									.With(new Layer(4, 4))
-									.With(new Layer(4, 2));
+			var layers = new LayerList()
+							.WithLayer(1).From(3).To(4)
+							.WithLayer(2).From(4).To(4)
+							.WithLayer(3).From(4).To(2);
 
 			Assert.Throws<ArgumentOutOfRangeException>(() => layers[0].Count());
 			layers[1].Count().Should().Be(12);
