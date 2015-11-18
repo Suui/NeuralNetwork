27242e940571a0f5671d0e5e9a75a4413ad8df79
Suui <suui.gd@gmail.com>
2015-11-18 22:36:56 +0100

    Refactor parallel end moved class

--- Test/LayerListShould.cs
+++ Test/LayerListShould.cs
@@ -38,45 +38,4 @@ namespace Test
 			Assert.Throws<ArgumentOutOfRangeException>(() => layers[4].Count());
 		}
 	}
-
-	public class LayerListBuilder
-	{
-		private readonly LayerList _layerList;
-		private int _index;
-		private int _leftPerceptrons;
-
-		public LayerListBuilder()
-		{
-			_layerList = new LayerList();
-		}
-
-		public LayerListBuilder Build()
-		{
-			return this;
-		}
-
-		public LayerListBuilder WithLayer(int index)
-		{
-			_index = index;
-			return this;
-		}
-
-		public LayerListBuilder From(int leftPerceptrons)
-		{
-			_leftPerceptrons = leftPerceptrons;
-			return this;
-		}
-
-		public LayerListBuilder To(int rightPerceptrons)
-		{
-			var layer = new Layer(_index, _leftPerceptrons, rightPerceptrons);
-			_layerList.Layers.Add(layer);
-			return this;
-		}
-
-		public LayerList Get()
-		{
-			return _layerList;
-		}
-	}
 }
