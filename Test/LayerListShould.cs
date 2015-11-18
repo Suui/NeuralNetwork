34de5148dc0df97852752b481f5f30e40e68fc81
Suui <suui.gd@gmail.com>
2015-11-18 19:02:16 +0100

    Green: be_built_with_the_correct_indexes

---
+++ Test/LayerListShould.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using Source.Layers;
+
+namespace Test
+{
+	[TestFixture]
+	public class LayerListShould
+	{
+		[Test]
+		public void be_built_with_the_correct_indexes()
+		{
+			LayerList layers = new LayerList()
+									.With(new Layer(3, 4))
+									.With(new Layer(4, 4))
+									.With(new Layer(4, 2));
+
+			layers.Haslayer(0).Should().BeFalse();
+		}
+	}
+
+	public class LayerList
+	{
+		public List<Layer> Layers { get; set; }
+
+		public LayerList()
+		{
+			Layers = new List<Layer>();
+		}
+
+		public LayerList With(Layer layer)
+		{
+			Layers.Add(layer);
+			return this;
+		}
+
+		public bool Haslayer(int index)
+		{
+			return false;
+		}
+	}
+}
