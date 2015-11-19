55238b6d4e3e47cb9318e0736b845a16334fd230
Suui <suui.gd@gmail.com>
2015-11-20 00:06:51 +0100

    Green: return_an_exit_value_of_0_when_perceptrons_have_a_ver_low_threshold

--- Test/NeuralNetworkShould.cs
+++ Test/NeuralNetworkShould.cs
@@ -36,5 +36,20 @@ namespace Test
 
 			neuralNetwork.ExitValues[0].Should().Be(1.0);
 		}
+
+		[Test]
+		public void return_an_exit_value_of_0_when_perceptrons_have_a_ver_low_threshold()
+		{
+			var thresholdRandomizer = Substitute.For<ThresholdRandomizer>();
+			thresholdRandomizer.GetThreshold().Returns(-9999);
+			var neuralNetwork = new NeuralNetworkBuilder().Build()
+														  .WithLayer(1).From(1).To(1)
+														  .WithLayer(2).From(1).To(1)
+														  .Get();
+
+			neuralNetwork.Execute();
+
+			neuralNetwork.ExitValues[0].Should().Be(0.0);
+		}
 	}
 }
