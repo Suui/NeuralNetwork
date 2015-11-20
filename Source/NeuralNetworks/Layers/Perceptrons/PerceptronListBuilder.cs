46b7a96a09377b943ed6b3e78587de5a1887a1c1
Suui <suui.gd@gmail.com>
2015-11-20 19:22:39 +0100

    Red. Refactor parallel step removed unused

--- Source/NeuralNetworks/Layers/Perceptrons/PerceptronListBuilder.cs
+++ Source/NeuralNetworks/Layers/Perceptrons/PerceptronListBuilder.cs
@@ -6,28 +6,12 @@ namespace Source.NeuralNetworks.Layers.Perceptrons
 {
 	public class PerceptronListBuilder
 	{
-		public static PerceptronList Build(int numberOfPerceptrons, Layer previousLayer)
-		{
-			return previousLayer == null ? EntryPerceptronList(numberOfPerceptrons)
-										 : PerceptronList(numberOfPerceptrons, previousLayer);
-		}
-
 		private static PerceptronList EntryPerceptronList(int numberOfPerceptrons)
 		{
 			var perceptrons = new List<Perceptron>();
 
 			for (var i = 0; i < numberOfPerceptrons; i++)
 				perceptrons.Add(new EntryPerceptron(i));
-
-			return new PerceptronList(perceptrons);
-		}
-
-		private static PerceptronList PerceptronList(int numberOfPerceptrons, Layer previousLayer)
-		{
-			var perceptrons = new List<Perceptron>();
-
-			for (var i = 0; i < numberOfPerceptrons; i++)
-				perceptrons.Add(new Perceptron(i, previousLayer));
 
 			return new PerceptronList(perceptrons);
 		}
