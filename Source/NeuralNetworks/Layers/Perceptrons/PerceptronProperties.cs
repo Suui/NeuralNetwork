c5007a2b606d09c6b2f8cef1edda257d80439d4f
Suui <suui.gd@gmail.com>
2015-11-21 21:40:49 +0100

    Red. Refactor parallel step added PerceptronProperties ctor

--- Source/NeuralNetworks/Layers/Perceptrons/PerceptronProperties.cs
+++ Source/NeuralNetworks/Layers/Perceptrons/PerceptronProperties.cs
@@ -2,8 +2,14 @@ using Source.NeuralNetworks.Thresholds;
 
 namespace Source.NeuralNetworks.Layers.Perceptrons
 {
-	public struct PerceptronProperties
+	public class PerceptronProperties
 	{
+		public PerceptronProperties(ThresholdGenerator thresholdGenerator)
+		{
+			IsEntryPerceptronList = false;
+			ThresholdGenerator = thresholdGenerator;
+		}
+
 		public bool IsEntryPerceptronList { get; set; }
 		public Layer PreviousLayer { get; set; }
 		public ThresholdGenerator ThresholdGenerator { get; set; }
