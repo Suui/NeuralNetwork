d10da304c4b647d1674480c254e374362456757c
Suui <suui.gd@gmail.com>
2015-11-22 01:32:22 +0100

    Refactor parallel step removed unused

--- Source/NeuralNetworks/Layers/Connections/ConnectionListBuilder.cs
+++ Source/NeuralNetworks/Layers/Connections/ConnectionListBuilder.cs
@@ -2,21 +2,6 @@
 {
 	public class ConnectionListBuilder
 	{
-		public static ConnectionList Build(int leftPerceptrons, int rightPerceptrons)
-		{
-			var connectionList = new ConnectionList();
-
-			for (int i = 0; i < leftPerceptrons; i++)
-			{
-				for (int j = 0; j < rightPerceptrons; j++)
-				{
-					connectionList.Add(new Connection(i + 1, j + 1));
-				}
-			}
-
-			return connectionList;
-		}
-
 		public static ConnectionList Build(int leftPerceptrons, int rightPerceptrons, ConnectionProperties connectionProperties)
 		{
 			var connectionList = new ConnectionList();
