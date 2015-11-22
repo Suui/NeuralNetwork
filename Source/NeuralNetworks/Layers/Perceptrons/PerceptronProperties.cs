d0bf0ac8532f48993181d587025cd414b636d88c
Suui <suui.gd@gmail.com>
2015-11-22 01:14:42 +0100

    Cleanup

--- Source/NeuralNetworks/Layers/Perceptrons/PerceptronProperties.cs
+++ Source/NeuralNetworks/Layers/Perceptrons/PerceptronProperties.cs
@@ -5,14 +5,14 @@ namespace Source.NeuralNetworks.Layers.Perceptrons
 {
 	public class PerceptronProperties
 	{
-		public PerceptronProperties(RandomGenerator thresholdGenerator)
+		public bool IsEntryPerceptronList { get; set; }
+		public Layer PreviousLayer { get; set; }
+		public NumberGenerator ThresholdGenerator { get; set; }
+
+		public PerceptronProperties(NumberGenerator thresholdGenerator)
 		{
 			IsEntryPerceptronList = false;
 			ThresholdGenerator = thresholdGenerator;
 		}
-
-		public bool IsEntryPerceptronList { get; set; }
-		public Layer PreviousLayer { get; set; }
-		public RandomGenerator ThresholdGenerator { get; set; }
 	}
 }
