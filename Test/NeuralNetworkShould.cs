f47a70b0b0272c6eacb28de458040967ce5d76e0
Suui <suui.gd@gmail.com>
2015-11-22 01:24:03 +0100

    Refactor parallel step using new ctor

--- Test/NeuralNetworkShould.cs
+++ Test/NeuralNetworkShould.cs
@@ -3,6 +3,7 @@ using FluentAssertions;
 using NSubstitute;
 using NUnit.Framework;
 using Source.NeuralNetworks;
+using Source.NeuralNetworks.Layers.Connections;
 using Source.NeuralNetworks.Layers.Perceptrons;
 using Source.NeuralNetworks.NumberGenerators;
 
@@ -13,18 +14,21 @@ namespace Test
 	public class NeuralNetworkShould
 	{
 		private NumberGenerator _thresholdGenerator;
+		private NumberGenerator _weightGenerator;
 
 		[SetUp]
 		public void given_a_threshold_generator()
 		{
 			_thresholdGenerator = Substitute.For<DelimitedRandom>(0, 0);
+			_weightGenerator = Substitute.For<DelimitedRandom>(0, 0);
 		}
 
 		[Test]
 		public void return_an_exit_value_of_1_when_perceptrons_have_a_very_high_threshold()
 		{
 			_thresholdGenerator.Generate().Returns(9999.0);
-			var neuralNetwork = new NeuralNetworkBuilder(new PerceptronProperties(_thresholdGenerator))
+			_weightGenerator.Generate().Returns(1.0);
+			var neuralNetwork = new NeuralNetworkBuilder(new ConnectionProperties(_weightGenerator), new PerceptronProperties(_thresholdGenerator))
 								.WithLayer(1).From(1).To(1)
 								.WithLayer(2).From(1).To(1)
 								.Build();
@@ -38,7 +42,8 @@ namespace Test
 		public void return_an_exit_value_of_0_when_perceptrons_have_a_very_low_threshold()
 		{
 			_thresholdGenerator.Generate().Returns(-9999.0);
-			var neuralNetwork = new NeuralNetworkBuilder(new PerceptronProperties(_thresholdGenerator))
+			_weightGenerator.Generate().Returns(1.0);
+			var neuralNetwork = new NeuralNetworkBuilder(new ConnectionProperties(_weightGenerator), new PerceptronProperties(_thresholdGenerator))
 								.WithLayer(1).From(1).To(1)
 								.WithLayer(2).From(1).To(1)
 								.Build();
@@ -52,7 +57,8 @@ namespace Test
 		public void return_0_point_5_when_it_has_no_hidden_layers_entry_value_is_0_threshold_is_0_and_weight_is_1()
 		{
 			_thresholdGenerator.Generate().Returns(0.0);
-			var neuralNetwork = new NeuralNetworkBuilder(new PerceptronProperties(_thresholdGenerator))
+			_weightGenerator.Generate().Returns(1.0);
+			var neuralNetwork = new NeuralNetworkBuilder(new ConnectionProperties(_weightGenerator), new PerceptronProperties(_thresholdGenerator))
 								.WithLayer(1).From(1).To(1)
 								.Build();
 
@@ -66,7 +72,8 @@ namespace Test
 		public void return_0_point_26894142_when_it_has_no_hidden_layers_entry_values_are_2_and_4_threshold_is_minus7_and_weights_are_1()
 		{
 			_thresholdGenerator.Generate().Returns(-7.0);
-			var neuralNetwork = new NeuralNetworkBuilder(new PerceptronProperties(_thresholdGenerator))
+			_weightGenerator.Generate().Returns(1.0);
+			var neuralNetwork = new NeuralNetworkBuilder(new ConnectionProperties(_weightGenerator), new PerceptronProperties(_thresholdGenerator))
 								.WithLayer(1).From(2).To(1)
 								.Build();
 
