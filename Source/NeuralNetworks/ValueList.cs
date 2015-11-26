501b71740958c689358f54ad28b5632015745939
Suui <suui.gd@gmail.com>
2015-11-26 19:08:16 +0100

    Implemented ValueList

---
+++ Source/NeuralNetworks/ValueList.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Source.NeuralNetworks
+{
+	public class ValueList<T> : IEnumerable
+	{
+		private readonly List<T> _valueList;
+
+		public ValueList()
+		{
+			_valueList = new List<T>();
+		}
+
+		public T this[int index] => _valueList[index - 1];
+
+		public IEnumerator GetEnumerator()
+		{
+			return _valueList.GetEnumerator();
+		}
+	}
+}
