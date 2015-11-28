30b6dab993ad1784d6bc41398eef827e34c80997
Suui <suui.gd@gmail.com>
2015-11-28 23:45:41 +0100

    Implemented LabelsReader

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using Source.ByteFileReaders;
+using Source.NeuralNetworks;
+
 
 namespace Source
 {
@@ -6,6 +8,10 @@ namespace Source
 	{
 		static void Main(string[] args)
 		{
+			var neuralNetwork = NeuralNetworkFactory.NeuralNetworkForPracticeFour();
+
+			var labelsReader = new LabelsReader(@"D:\Projects\Programming\C#\NeuralNetwork\DataMNIST\train_labels");
+			neuralNetwork.ExpectedExitValues = labelsReader.Next();
 		}
 	}
 }
