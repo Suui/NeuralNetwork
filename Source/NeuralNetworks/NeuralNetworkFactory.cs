ba1f7b4ea46155b23d90d9dd298c14d183b2e49e
Suui <suui.gd@gmail.com>
2015-11-28 03:10:54 +0100

    Implemented construction of the practice four NeuralNetwork

--- Source/NeuralNetworks/NeuralNetworkFactory.cs
+++ Source/NeuralNetworks/NeuralNetworkFactory.cs
@@ -1,15 +1,22 @@
-﻿using Source.NeuralNetworks.Layers;
+﻿using Source.NeuralNetworks.Layers.Connections;
+using Source.NeuralNetworks.Layers.Perceptrons;
+using Source.NumberGenerators;
 
 
 namespace Source.NeuralNetworks
 {
 	public class NeuralNetworkFactory
 	{
-		public static NeuralNetwork BuildNeuralNetworkForPractice()
+		public static NeuralNetwork NeuralNetworkForPracticeFour()
 		{
-			LayerDictionary layers = new LayerDictionary();
+			var weightGenerator = new DelimitedRandom(-10000, 10000);
+			var thresholdGenerator = new DelimitedRandom(-10000, 10000);
 
-			return new NeuralNetwork(layers);
-		}
+			return new NeuralNetworkBuilder(new ConnectionProperties(weightGenerator), new PerceptronProperties(thresholdGenerator))
+						.WithLayer(1).From(784).To(392)
+						.WithLayer(2).From(392).To(196)
+						.WithLayer(1).From(196).To(1)
+						.Build();
+        }
 	}
 }
