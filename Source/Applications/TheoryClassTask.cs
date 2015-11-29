5da1e2d8c654985589a3c122fd4ac59a3eab9d20
Suui <suui.gd@gmail.com>
2015-11-30 00:05:11 +0100

    Implemented full task with various layers

--- Source/Applications/TheoryClassTask.cs
+++ Source/Applications/TheoryClassTask.cs
@@ -1,4 +1,10 @@
+using System;
 using System.IO;
+using Source.ByteFileReaders;
+using Source.NeuralNetworks;
+using Source.NeuralNetworks.Layers.Connections;
+using Source.NeuralNetworks.Layers.Perceptrons;
+using Source.NumberGenerators;
 
 
 namespace Source.Applications
@@ -7,11 +13,56 @@ namespace Source.Applications
 	{
 		public override void Execute()
 		{
-			const string fileName = "inputs";
-			CreateByteFile(fileName);
+			CreateInputs("inputs");
+			CreateExpectedOutputs("expected_outputs");
+
+			var neuralNetwork = new NeuralNetworkBuilder(new ConnectionProperties(new DelimitedRandom(-1, 1)), 
+								new PerceptronProperties(new DelimitedRandom(-1, 1)))
+								.WithLayer(1).From(9).To(9)
+								.WithLayer(2).From(9).To(6)
+								.WithLayer(3).From(6).To(3)
+								.WithLayer(4).From(3).To(1)
+								.Build();
+
+			var errorCalculator = new ErrorCalculator();
+			var inputReader = new ByteFileReader("inputs", 0);
+			var expectedOutputReader = new ByteFileReader("expected_outputs", 0);
+
+			for (var i = 0; i < 54*1000; i++)
+			{
+				neuralNetwork.EntryValues = inputReader.Next(9);
+				neuralNetwork.ExpectedExitValues = expectedOutputReader.Next();
+
+				neuralNetwork.Execute();
+				neuralNetwork.ExecuteBackPropagation();
+
+				errorCalculator.AddResult(neuralNetwork.ExpectedExitValues[1], neuralNetwork.ExitValues[1]);
+				Console.WriteLine("MSE for iteration " + i + " = " + errorCalculator.GetMSE());
+
+				if (i % 54 == 0)
+				{
+					inputReader.Close();
+					expectedOutputReader.Close();
+					inputReader = new ByteFileReader("inputs", 0);
+					expectedOutputReader = new ByteFileReader("expected_outputs", 0);
+				}
+			}
 		}
 
-		private void CreateByteFile(string fileName)
+		private void CreateExpectedOutputs(string fileName)
+		{
+			if (File.Exists(fileName)) return;
+
+			var fileStream = new FileStream(fileName, FileMode.Create);
+			fileStream.WriteByte(1);
+			fileStream.WriteByte(1);
+			fileStream.WriteByte(1);
+			fileStream.WriteByte(0);
+			fileStream.WriteByte(0);
+			fileStream.WriteByte(0);
+		}
+
+		private void CreateInputs(string fileName)
 		{
 			if (File.Exists(fileName)) return;
 
@@ -75,6 +126,8 @@ namespace Source.Applications
 			fileStream.WriteByte(0);
 			fileStream.WriteByte(0);
 			fileStream.WriteByte(1);
+
+			fileStream.Close();
 		}
 	}
 }
