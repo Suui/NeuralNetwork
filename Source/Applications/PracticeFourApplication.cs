70268d9522310c8b177287db3e8aeaba278f386e
Suui <suui.gd@gmail.com>
2015-11-29 23:02:47 +0100

    Refactor extracted classes

---
+++ Source/Applications/PracticeFourApplication.cs
@@ -0,0 +1,30 @@
+using System;
+using Source.ByteFileReaders;
+using Source.NeuralNetworks;
+
+
+namespace Source.Applications
+{
+	public class PracticeFourApplication : CustomApplication
+	{
+		public override void Execute()
+		{
+			var neuralNetwork = NeuralNetworkFactory.NeuralNetworkForPracticeFour();
+			var errorCalculator = new ErrorCalculator();
+
+			var labelsReader = new ByteFileReader(@"D:\Projects\Programming\C#\NeuralNetwork\DataMNIST\train_labels", 8);
+			var imagesReader = new ByteFileReader(@"D:\Projects\Programming\C#\NeuralNetwork\DataMNIST\train_images", 16);
+			for (int i = 0; i < 60000; i++)
+			{
+				neuralNetwork.ExpectedExitValues = labelsReader.Next();
+				neuralNetwork.EntryValues = imagesReader.Next(784);
+
+				neuralNetwork.Execute();
+				neuralNetwork.ExecuteBackPropagation();
+
+				errorCalculator.AddResult(neuralNetwork.ExpectedExitValues[1], neuralNetwork.ExitValues[1]);
+				Console.WriteLine("MSE for iteration " + i + " = " + errorCalculator.GetMSE());
+			}
+		}
+	}
+}
--- Source/Program.cs
+++ Source/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Source.ByteFileReaders;
-using Source.NeuralNetworks;
+﻿using Source.Applications;
 
 
 namespace Source
@@ -9,22 +7,8 @@ namespace Source
 	{
 		static void Main(string[] args)
 		{
-			var neuralNetwork = NeuralNetworkFactory.NeuralNetworkForPracticeFour();
-			var errorCalculator = new ErrorCalculator();
-
-			var labelsReader = new ByteFileReader(@"D:\Projects\Programming\C#\NeuralNetwork\DataMNIST\train_labels", 8);
-			var imagesReader = new ByteFileReader(@"D:\Projects\Programming\C#\NeuralNetwork\DataMNIST\train_images", 16);
-			for (int i = 0; i < 60000; i++)
-			{
-				neuralNetwork.ExpectedExitValues = labelsReader.Next();
-				neuralNetwork.EntryValues = imagesReader.Next(784);
-
-				neuralNetwork.Execute();
-				neuralNetwork.ExecuteBackPropagation();
-
-				errorCalculator.AddResult(neuralNetwork.ExpectedExitValues[1], neuralNetwork.ExitValues[1]);
-				Console.WriteLine("MSE for iteration " + i + " = " + errorCalculator.GetMSE());
-			}
+			var application = new PracticeFourApplication();
+			application.Execute();
 		}
 	}
 }
