8f34d3e8cf9a15bb6718056174b10ac9f73cf923
Suui <suui.gd@gmail.com>
2015-12-01 14:51:52 +0100

    Green: return_true_when_passed_number_equals_expected_number

---
+++ Source/AcceptanceMatchers/AcceptanceMatcher.cs
@@ -0,0 +1,27 @@
+﻿namespace Source.AcceptanceMatchers
+{
+	public class AcceptanceMatcher
+	{
+		private readonly double _belowDistance;
+		private readonly double _aboveDistance;
+		private double _value;
+
+		public AcceptanceMatcher(double belowDistance, double aboveDistance)
+		{
+			_belowDistance = belowDistance;
+			_aboveDistance = aboveDistance;
+		}
+
+		public AcceptanceMatcher IsValue(double value)
+		{
+			_value = value;
+			return this;
+		}
+
+		public bool AcceptedForExpectedValue(double expectedValue)
+		{
+			if (_value == expectedValue) return true;
+			return false;
+		}
+	}
+}
---
+++ Test/AcceptanceMatcherShould.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Source.AcceptanceMatchers;
+
+
+/*
+	TODO
+	We pass a two numbers to it, an expected value and a value we are checking that is
+	near enough the expected one.
+
+	Tests
+	0, 0 => true
+	2, 2 => true
+	2, 2 + maxDistance => true
+	2, 2 - minDistance => true
+	2, 2 + maxDistance + 0000.1 => false
+	2, 2 - maxDistance - 0000.1 => false
+*/
+
+
+namespace Test
+{
+	[TestFixture]
+	public class AcceptanceMatcherShould
+	{
+		private AcceptanceMatcher _acceptanceMatcher;
+
+		[SetUp]
+		public void Initialize()
+		{
+			_acceptanceMatcher = new AcceptanceMatcher(-0.049, 0.049);
+		}
+
+		[Test]
+		public void return_true_when_passed_number_equals_expected_number()
+		{
+			_acceptanceMatcher.IsValue(0).AcceptedForExpectedValue(0).Should().BeTrue();
+		}
+	}
+}
