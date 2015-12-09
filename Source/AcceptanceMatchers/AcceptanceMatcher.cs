fa576a4a3823088753c8b55860fba186ac37f8be
Suui <suui.gd@gmail.com>
2015-12-09 01:13:54 +0100

    Refactor

--- Source/AcceptanceMatchers/AcceptanceMatcher.cs
+++ Source/AcceptanceMatchers/AcceptanceMatcher.cs
@@ -27,10 +27,7 @@
 
 		public bool Accepted()
 		{
-			if (_value == _expectedValue) return true;
-			if (_value <= _expectedValue + _aboveDistance && _value >= _expectedValue + _belowDistance) return true;
-			var debug = _expectedValue + _belowDistance;
-			return false;
+			return _value <= _expectedValue + _aboveDistance && _value >= _expectedValue + _belowDistance;
 		}
 	}
 }
