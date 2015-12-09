c9c3910c179dd0fb3fb659c3588229897f0cbaea
Suui <suui.gd@gmail.com>
2015-12-09 02:54:55 +0100

    Testing db

--- Source/Persistance/Database.cs
+++ Source/Persistance/Database.cs
@@ -1,3 +1,5 @@
+using System;
+using Npgsql;
 using Source.NeuralNetworks;
 
 
@@ -9,6 +11,32 @@ namespace Source.Persistance
 		{
 			var firstLayerWeights = neuralNetwork.GetWeightsForLayer(1);
 			var secondLayerWeights = neuralNetwork.GetWeightsForLayer(2);
+
+			var firstLayerThresholds = neuralNetwork.GetThresholdsForLayer(1);
+			var secondLayerThresholds = neuralNetwork.GetThresholdsForLayer(2);
+			var thirdLayerThresholds = neuralNetwork.GetThresholdsForLayer(3);
+
+			const string connectionString = "Host=127.0.0.1;" +
+											"Username=intelligent_systems;" +
+											"Password=intelligent_systems;" +
+											"Database=intelligent_systems;";
+
+			try
+			{
+				var connection = new NpgsqlConnection(connectionString);
+				connection.Open();
+				var command = new NpgsqlCommand { Connection = connection };
+
+				for (var i = 1; i <= firstLayerThresholds.Count; i++)
+				{
+					command.CommandText = "UPDATE pr4_thresholds_layer1 SET threshold=" + firstLayerThresholds[i-1] + " WHERE id=" + i;
+					command.ExecuteNonQuery();
+				}
+			}
+			catch (Exception)
+			{
+				throw;
+			}
 		}
 	}
 }
