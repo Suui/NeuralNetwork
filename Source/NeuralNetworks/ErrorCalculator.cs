016238930dc809977e9eca49f534542cba6ba8b4
Suui <suui.gd@gmail.com>
2015-12-09 01:38:40 +0100

    Implemented Success percentage

--- Source/NeuralNetworks/ErrorCalculator.cs
+++ Source/NeuralNetworks/ErrorCalculator.cs
@@ -8,20 +8,28 @@ namespace Source.NeuralNetworks
 	{
 		private double _minimumSquaredError = 0.0;
 		private double _numberOfResults = 0.0;
+		private double _numberOfSuccesses = 0.0;
 		private readonly AcceptanceMatcher _acceptanceMatcher = new AcceptanceMatcher(-0.049, 0.049);
 
 		public void AddResult(double expectedExitValue, double exitValue)
 		{
-			_minimumSquaredError += _acceptanceMatcher
-									.ForExpectedValue(expectedExitValue)
-									.IsValue(exitValue).Accepted() ? 0.0
-																   : Math.Pow(expectedExitValue - exitValue, 2);
+			var error = _acceptanceMatcher
+						.ForExpectedValue(expectedExitValue)
+						.IsValue(exitValue).Accepted() ? 0.0
+													   : Math.Pow(expectedExitValue - exitValue, 2);
+			if (error == 0.0) _numberOfSuccesses += 1;
+			_minimumSquaredError += error;
 			_numberOfResults += 1;
 		}
 
 		public double GetMSE()
 		{
 			return 1 / _numberOfResults * _minimumSquaredError;
+		}
+
+		public double GetSuccessPercentage()
+		{
+			return _numberOfSuccesses / _numberOfResults * 100.0;
 		}
 	}
 }
